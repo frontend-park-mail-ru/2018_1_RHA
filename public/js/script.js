3c6ecffe088fd5ef3e3215fc6a65ce27d3c5c09b
HimmelSpark <adam404pet@gmail.com>
2018-02-12 23:40:50 +0300

    just commit

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,26 @@
 $(document).ready( () => $('#body').fadeIn('slow'));
 
 
-$("#play").click( () => {
-    $("#play").fadeOut('slow');
-    window.location.href='#openModal';
+// $("#close").click( () => {
+//     $("#play").fadeIn('slow');
+//     window.location.href='#';
+//     document.getElementById("openModal").style.opacity = "0";
+// });
+
+// const play = () => {
+// 	document.getElementById("play").style.opacity = "0";
+// 	window.location.href='#openModal';
+//     document.getElementById("openModal").style.opacity = "1";
+// }
+
+const close = () => {
+	document.getElementById("play").style.opacity = "1";
+	window.location.href = '#';
+	document.getElementById("openModal").style.opacity = "0";
+}
+
+document.getElementById("play").onclick( () => {
+	document.getElementById("play").style.opacity = "0";
+	window.location.href='#openModal';
     document.getElementById("openModal").style.opacity = "1";
-
 });
-
-$("#close").click( () => {
-    $("#play").fadeIn('slow');
-    window.location.href='#';
-    document.getElementById("openModal").style.opacity = "0";
-});
-
-
