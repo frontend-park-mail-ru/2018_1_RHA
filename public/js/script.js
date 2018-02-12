423c920cea44f5f389d02f3da427ce3f26b168fa
supreme2302 <chuvashov.egor@mail.ru>
2018-02-12 19:43:47 +0300

    fadeIn modal window and fix placeholder

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,17 @@
 $(document).ready( () => $('#body').fadeIn('slow'));
 
 
-$("#play").click( () => $("#play").fadeOut('slow'));
+$("#play").click( () => {
+    $("#play").fadeOut('slow');
+    window.location.href='#openModal';
+    document.getElementById("openModal").style.opacity = "1";
 
-$("#close").click( () => $("#play").fadeIn('slow'));
+});
+
+$("#close").click( () => {
+    $("#play").fadeIn('slow');
+    window.location.href='#';
+    document.getElementById("openModal").style.opacity = "0";
+});
+
+
