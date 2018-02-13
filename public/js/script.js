0830a1373ad96361b601c7c7d8b8e648147ced02
chapay <BritikovKI@Yandex.ru>
2018-02-13 17:48:25 +0300

    new one

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,18 @@
 $(document).ready( () => $('#body').fadeIn('slow'));
 
 
-$("#play").click( () => {
-    $("#play").fadeOut('slow');
+const play = () => {
+    document.getElementById("play").style.opacity = "0";
     window.location.href='#openModal';
     document.getElementById("openModal").style.opacity = "1";
+}
 
-});
-
-$("#close").click( () => {
-    $("#play").fadeIn('slow');
-    window.location.href='#';
+const close = () => {
+    document.getElementById("play").style.opacity = "1";
+    window.location.href = '#';
     document.getElementById("openModal").style.opacity = "0";
-});
+}
+
+
 
 
