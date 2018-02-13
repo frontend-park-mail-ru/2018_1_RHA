4b3b8de7a1f8fc5624063e4216ee2d45712342e4
HimmelSpark <adam404pet@gmail.com>
2018-02-13 11:42:00 +0300

    mite

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,38 @@
+// event handler for play
+const play = () => {
+    document.getElementById("play").style.opacity = 0;
+    document.getElementById("openModal").style.opacity = 1;
+    window.location.href = '#openModal';
+};
 
-$(document).ready( () => $('#body').fadeIn('slow'));
+// event listener for play
+document.getElementById("play").addEventListener('click', play);
 
 
-$("#play").click( () => $("#play").fadeOut('slow'));
 
-$("#close").click( () => $("#play").fadeIn('slow'));
+// event handler for close
+const close = () => {
+    document.getElementById("play").style.opacity = 1;
+    document.getElementById("openModal").style.opacity = 0;
+    window.location.href = '#';
+};
+
+// event listener for close
+document.getElementById("close").addEventListener('click', close);
+
+
+const mute = () => {
+    let video = document.getElementsByTagName('video')[0];
+    if( video.muted === true ){
+        video.muted = '';
+        document.getElementById('mute').innerHTML = 'Sound OFF';
+    }
+    else {
+       video.muted = 'muted';
+       document.getElementById('mute').innerHTML = 'Sound ON';
+    }
+
+};
+
+document.getElementById("mute").addEventListener('click', mute);
+
