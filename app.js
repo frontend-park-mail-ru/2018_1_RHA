92a52b3c99851dad8f6c9a4a52563fc5f5893581
HimmelSpark <adam404pet@gmail.com>
2018-02-16 19:17:22 +0300

    app fix

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
 const express = require("express"); // express
-
-const fs = require("fs"); // логирование
-
 const bodyParser = require("body-parser"); // обработка форм
-
 const app = express(); // объект приложения
+const port = process.env.PORT || 3000;
 
-const urlencodedParser = bodyParser.urlencoded({extended: false});
-
-// app.post("/register", urlencodedParser, (request, response) => {
-//
-//     if( !request.body ) return response.sendStatus(400);
-//
-//     console.log(request.body);
-//
-//   response.send(`${request.body.userName} - ${request.body.userAge}`);
-//
-// });
-
+app.use( (req, res, next) => { console.log(req,res); next(); } );
 app.use(express.static(__dirname + "/public")); // middleware
-
-app.listen(3000); // слушаем порт
+app.listen(port); // слушаем порт
