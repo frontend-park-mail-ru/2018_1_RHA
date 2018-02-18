9b70efd2d8b02ee5aebdd890ca748e845d004c54
HimmelSpark <adam404pet@gmail.com>
2018-02-18 20:34:15 +0300

    modal change

--- app.js
+++ app.js
@@ -6,3 +6,38 @@ const port = process.env.PORT || 3000;
 app.use( (req, res, next) => { console.log(req,res); next(); } );
 app.use(express.static(__dirname + "/public")); // middleware
 app.listen(port); // слушаем порт
+
+
+const users = {};
+const ids = {};
+
+app.post('/auth', (req, res) => {
+   const username = req.body.username;
+   const email = req.body.email;
+   if (!username || !username) {
+       return res.status(400).end();
+   }
+   if (!users[email]) {
+       users[email] = {
+           username,
+           email,
+           count: 0,
+       };
+   }
+   const id = uuid();
+   ids[id] = email;
+
+   res.cookie('smth', id, {domain: 'localhost', expires: new Date(Date.now()) + 1000 * 60 * 10});
+   res.json({id});
+});
+
+app.get('/me', (req, res) => {
+   const id = req.cookies['smth'];
+   const email = ids[id];
+   if (!email || !users[email]) {
+       return res.status(401).end();
+   }
+
+   users[email].count += 1;
+   res.json(users[email]);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -8,6 +8,9 @@ const reg_form = document.getElementById('reg_form');
 const video = document.getElementsByTagName('video')[0];
 const mute_obj = document.getElementById('mute');
 
+const log_butt = document.getElementById('log_butt');
+const reg_butt = document.getElementById('reg_butt');
+
 //-----------------------------
 
 const relay = () => {
@@ -19,26 +22,66 @@ close_modal.addEventListener('click', relay);
 play.addEventListener('click', relay);
 
 
-const mute = () => {
+mute_obj.addEventListener('click', (event) => {
     if( video.muted === true ){
         mute_obj.innerHTML = 'Sound ON';
         video.muted = '';
     }
     else {
-       mute_obj.innerHTML = 'Sound OFF';
-       video.muted = 'muted';
+        mute_obj.innerHTML = 'Sound OFF';
+        video.muted = 'muted';
     }
-};
+});
 
-mute_obj.addEventListener('click', mute);
+log_butt.addEventListener('click', (event) => {
 
-
-const switch_log = () => {
     login_form.style.display = 'block';
+    log_butt.style.display = 'none';
+
     reg_form.style.display = 'none';
+    reg_butt.style.display = 'block';
+});
+
+reg_butt.addEventListener('click', (event) => {
+
+    login_form.style.display = 'none';
+    log_butt.style.display = 'block';
+
+    reg_form.style.display = 'block';
+    reg_butt.style.display = 'none';
+});
+
+//------------------------------------
+
+const auth = (username, email, callback) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/auth', true);
+    xhr.withCredentials = true;
+
+    const user = {username, email};
+    const body = JSON.stringify(user);
+
+    xhr.setRequestHeader('Content-Type','application/json charset=utf8');
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState !== 4) return;
+        if (xhr.status >= 300) {
+            return callback(xhr, null);
+        }
+
+        const response = JSON.parse(xhr.responseText);
+        callback(null, response);
+    };
+    xhr.send(body);
 };
 
-const switch_reg = () => {
-    login_form.style.display = 'none';
-    reg_form.style.display = 'block';
-};
+login_form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(login_form.elements);
+    const email = login_form.elements['email'].value;
+    const username = login_form.elements['username'].value;
+
+    auth(username, email, (err, resp) => {
+       console.log(err, resp);
+    });
+});
