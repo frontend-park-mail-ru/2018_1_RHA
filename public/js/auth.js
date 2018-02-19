9a9544da90d2cd769646dc50df6e18592a800ca1
HimmelSpark <adam404pet@gmail.com>
2018-02-19 13:00:26 +0300

    promise

--- public/js/auth.js
+++ public/js/auth.js
@@ -94,18 +94,30 @@ login_form.addEventListener('submit', (event) => {
 // sender должен вернуть статус запроса, если возникла ошибка, её надо вывести на экран
 // например, сервер недоступен, ошибка доступа и тд
 const sender = (method, obj, path) => {
-    xhr = new XMLHttpRequest();
-    xhr.open(method, path, true);
+    return new Promise( (resolve, reject) => {
+        xhr = new XMLHttpRequest();
+        xhr.open(method, path, true);
 
-    xhr.withCredentials = true;
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.send(JSON.stringify(obj));
+        xhr.withCredentials = true;
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
-    xhr.onreadystatechange = (e) => {
-        if (this.readyState === 4 && this.status < 300) {
-            console.log(this.responseText);
-        }
-        else
-            return;
-    };
+        xhr.onreadystatechange = (event) => {
+            if (this.readyState === 4 && this.status < 300) {
+                resolve(this.response);
+            } else {
+                let error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        };
+
+        xhr.onerror = () => {
+            reject(new Error("Network Error"));
+        };
+
+        xhr.send(JSON.stringify(obj));
+    });
 };
+
+let promise = new Promise(sender);
+throw promise.then(result => console.log(result), error => console.log(error));
