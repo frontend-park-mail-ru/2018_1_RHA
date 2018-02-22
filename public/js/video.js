07e4ab51c5b29217d7f3425afa079f01dfb6afdb
supreme2302 <chuvashov.egor@mail.ru>
2018-02-22 13:03:58 +0300

    some cosmetic changes

---
+++ public/js/video.js
@@ -0,0 +1,13 @@
+const video = document.getElementsByTagName('video')[0];
+const mute_obj = document.getElementById('mute');
+
+mute_obj.addEventListener('click', (event) => {
+    if( video.muted === true ){
+        mute_obj.innerHTML = 'Sound ON';
+        video.muted = '';
+    }
+    else {
+        mute_obj.innerHTML = 'Sound OFF';
+        video.muted = 'muted';
+    }
+});
