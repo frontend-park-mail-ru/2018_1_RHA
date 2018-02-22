00e08d912f776f743c9720e3101c23f1c178b94e
chapay <BritikovKI@Yandex.ru>
2018-02-22 17:20:42 +0300

    Few more connections

--- public/js/auth.js
+++ public/js/auth.js
@@ -21,6 +21,9 @@ reg_part.addEventListener('submit', (event) => {
     const email = lines[1]['value'];
 
     const newUser = {username: username, email: email, password: password};
+    open_modal.classList.toggle('hidden');
+    menu.classList.toggle('hidden');
+    toHideOnClose = open_modal;
 });
 
 login_part.addEventListener('submit', (event) => {
@@ -31,35 +34,38 @@ login_part.addEventListener('submit', (event) => {
    const user = {username: username, email: email};
 
    let result = sender('POST', user, '/login');
+    open_modal.classList.toggle('hidden');
+    menu.classList.toggle('hidden');
+    toHideOnClose = open_modal;
 });
 
 // sender должен вернуть статус запроса, если возникла ошибка, её надо вывести на экран
 // например, сервер недоступен, ошибка доступа и тд
-const sender = (method, obj, path) => {
-    return new Promise( (resolve, reject) => {
-        xhr = new XMLHttpRequest();
-        xhr.open(method, path, true);
-
-        xhr.withCredentials = true;
-        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-
-        xhr.onreadystatechange = (event) => {
-            if (this.readyState === 4 && this.status < 300) {
-                resolve(this.response);
-            } else {
-                let error = new Error(this.statusText);
-                error.code = this.status;
-                reject(error);
-            }
-        };
-
-        xhr.onerror = () => {
-            reject(new Error("Network Error"));
-        };
-
-        xhr.send(JSON.stringify(obj));
-    });
-};
-
-let promise = new Promise(sender);
-throw promise.then(result => console.log(result), error => console.log(error));
+// const sender = (method, obj, path) => {
+//     return new Promise( (resolve, reject) => {
+//         xhr = new XMLHttpRequest();
+//         xhr.open(method, path, true);
+//
+//         xhr.withCredentials = true;
+//         xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+//
+//         xhr.onreadystatechange = (event) => {
+//             if (this.readyState === 4 && this.status < 300) {
+//                 resolve(this.response);
+//             } else {
+//                 let error = new Error(this.statusText);
+//                 error.code = this.status;
+//                 reject(error);
+//             }
+//         };
+//
+//         xhr.onerror = () => {
+//             reject(new Error("Network Error"));
+//         };
+//
+//         xhr.send(JSON.stringify(obj));
+//     });
+// };
+//
+// let promise = new Promise(sender);
+// throw promise.then(result => console.log(result), error => console.log(error));
