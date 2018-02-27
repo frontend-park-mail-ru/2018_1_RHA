0e98b6ca2379eb05d8d8d819adbdcb29be310810
supreme2302 <chuvashov.egor@mail.ru>
2018-02-27 20:21:48 +0300

    added network.js

---
+++ public/js/network.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function sendForm() {
+   // const formData = new FormData(form);
+    let object = {
+        name: document.getElementById('nickname-up').value,
+        email: document.getElementById('email-up').value,
+        password: document.getElementById('password-up').value
+    };
+    // formData.forEach((value, key) => {
+    //     object[key] = value;
+    // });
+    const json = JSON.stringify(object);
+    console.log(json);
+    const xhr = new XMLHttpRequest();
+    //xhr.open("POST", form.action, true);
+    xhr.open("POST", "http://localhost:5000/users/create", true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    xhr.withCredentials = true;
+    xhr.send(json);
+}
+
