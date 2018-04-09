27d3b93dfdc967f2d111fec3cf4f251264b6cf39
supreme2302 <chuvashov.egor@mail.ru>
2018-04-09 12:15:25 +0300

    s*ka

--- public/js/components/views/gameView/gameView.js
+++ public/js/components/views/gameView/gameView.js
@@ -1,78 +1,15 @@
-/* eslint-disable no-undef */
 import Section from '../baseView.js';
-import Hexagon from '../../../modules/graphics/hexagon.js';
-import inPoly from '../../../modules/game/core/core.js';
+import User from '../../../modules/userModel.js';
 
 export default class GameSection extends Section {
 	constructor () {
 		super();
-		this.parent = document.getElementById('game');
-		this.wrapper = document.createElement('div');
-		this.wrapper.innerHTML = generateCanvas(
-			{
-				'width': screen.width,
-				'height':screen.height
-			}
-		);
-		this.parent.appendChild(this.wrapper);
-		this.canvas = document.getElementById('game-canvas');
 	}
 
-	render() {
-
-		this.ctx = this.canvas.getContext('2d');
-		this.figures = [
-			{
-				name: 'hex1',
-				color: 'red',
-				figure: new Hexagon(this.ctx, 500, 500, 'red')
-			},
-			{
-				name: 'hex2',
-				color: 'yellow',
-				figure: new Hexagon(this.ctx, 650, 413.4, 'yellow')
-			},
-			{
-				name: 'hex3',
-				color: 'white',
-				figure: new Hexagon(this.ctx, 650, 586.6, 'white')
-			},
-			{
-				name: 'hex4',
-				color: 'green',
-				figure: new Hexagon(this.ctx, 500, 326.8, 'green')
-			},
-			{
-				name: 'hex5',
-				color: 'cyan',
-				figure: new Hexagon(this.ctx, 800, 500, 'cyan')
-			}
-		];
-
-		this.canvas.addEventListener('click', event => {
-
-			this.figures.forEach( (obj) => {
-				if (inPoly(event.x, event.y, obj.figure.xp, obj.figure.yp)) {
-					//TODO::something click
-				}
-			});
-		});
-
-		this.canvas.addEventListener('mousemove', event => {
-
-			this.figures.forEach( (obj) => {
-				if (inPoly(event.x, event.y, obj.figure.xp, obj.figure.yp)) {
-					console.log('orange');
-					obj.figure.reDraw('orange');
-				}
-				else {
-					obj.figure.reStore();
-				}
-			});
-		});
-
-
-		return this.wrapper;
+	render(parent) {
+		this.gameHeader = document.createElement('h1');
+		this.gameHeader.innerText = 'Super Game!';
+		return this.gameHeader;
 	}
 	allowed() {
 		// return User.isAuthorized();
