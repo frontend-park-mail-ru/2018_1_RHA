67ba4b0f8fb69030c594ef740f95cb0eace6f8a4
HimmelSpark <adam404pet@gmail.com>
2018-04-30 21:54:18 +0300

    TODOSHKI для Егора

--- public/js/modules/game/animation/animationOverlay.js
+++ public/js/modules/game/animation/animationOverlay.js
@@ -1,3 +1,4 @@
+import {animate} from './animate.js';
 
 export const animationOverlay = (x , y) => {
 
@@ -29,11 +30,10 @@ export const animationOverlay = (x , y) => {
 		x + cw * 0.02 * 0.66,
 		y + cw * 0.02 * 0.66
 	);
-
 	animate(function(timePassed) {
 		diff = ((timePassed / 1000) * Math.PI*2*10).toFixed(2);
 		ctx.clearRect(0, 0, cw, ch);
-		ctx.lineWidth = 10;
+		ctx.lineWidth = 10; //TODO шириной радиуса
 		gradient.addColorStop(0, '#030101');
 		gradient.addColorStop(0.5, '#711615');
 		gradient.addColorStop(1, '#ee2357');
@@ -42,30 +42,15 @@ export const animationOverlay = (x , y) => {
 		gradientStroke.addColorStop(0.5, '#0d3634');
 		gradientStroke.addColorStop(1, '#ffffff');
 
-
 		ctx.fillStyle = gradient;
 		ctx.strokeStyle = gradientStroke;
-
 
 		ctx.beginPath();
 		ctx.arc(x, y, cw * 0.02, start, diff/10+start, false);
 		ctx.stroke();
-		ctx.fill();
+		// ctx.fill();
 	}, 1000);
 	setTimeout(() => {
 		wrapper.removeChild(wrapper.lastChild);
 	}, 1000);
-};
-
-
-const animate = (draw, duration) => {
-	const start = performance.now();
-	requestAnimationFrame(function animate(time) {
-		let timePassed = time - start;
-		if (timePassed > duration) timePassed = duration;
-		draw(timePassed);
-		if (timePassed < duration) {
-			requestAnimationFrame(animate);
-		}
-	});
 };
--- public/js/modules/game/animation/move/moveAnimation.js
+++ public/js/modules/game/animation/move/moveAnimation.js
@@ -1,28 +1,56 @@
 import {animate} from '../animate.js';
-
+//TODO вместо крестика должен рисоваться кружок
+//TODO неправильно отсчитываются координаты
+//TODO нарисовать стрелочку
 export const moveAnimation = (x, y, x2, y2) => {
+
 	const wrapper = document.getElementsByClassName('wrapper')[0];
+
 	const wrapAnim = document.createElement('div');
-	wrapAnim.classList.add('attack-animation');
+	wrapAnim.classList.add('overlay');
+
+	const canvas = document.createElement('canvas');
+	wrapAnim.appendChild(canvas);
+	canvas.setAttribute('id', 'overlay');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 	wrapper.appendChild(wrapAnim);
-	wrapAnim.innerHTML = generateAttack(
-		{
-			'width': window.innerWidth * 0.7,
-			'height': window.innerWidth * 0.525 * 0.83,
-			'id': 'attack-canvas'
-		}
-	);
-	const canvas = document.getElementById('attack-canvas');
 	const ctx = canvas.getContext('2d');
+	const start = 4.72;
 	const cw = ctx.canvas.width;
 	const ch = ctx.canvas.height;
-	const radius = cw * 0.03;
-	let beginAngle = 0;
-	animate(() => {
+	let diff;
+	const gradient = ctx.createLinearGradient(
+		x - cw * 0.02 * 0.66,
+		y - cw * 0.02 * 0.66,
+		x + cw * 0.02 * 0.66,
+		y + cw * 0.02 * 0.66
+	);
+	const gradientStroke = ctx.createLinearGradient(
+		x - cw * 0.02 * 0.66,
+		y - cw * 0.02 * 0.66,
+		x + cw * 0.02 * 0.66,
+		y + cw * 0.02 * 0.66
+	);
+	animate(function(timePassed) {
+		diff = ((timePassed / 1000) * Math.PI*2*10).toFixed(2);
 		ctx.clearRect(0, 0, cw, ch);
-		ctx.lineWidth = 10;
-		ctx.arc(x, y, radius, beginAngle, (beginAngle + 0.1)*Math.PI);
-		beginAngle += 0.1;
+		ctx.lineWidth = 10; //TODO шириной радиуса
+		gradient.addColorStop(0, '#030101');
+		gradient.addColorStop(0.5, '#711615');
+		gradient.addColorStop(1, '#ee2357');
+
+		gradientStroke.addColorStop(0, '#030101');
+		gradientStroke.addColorStop(0.5, '#0d3634');
+		gradientStroke.addColorStop(1, '#ffffff');
+
+		ctx.fillStyle = gradient;
+		ctx.strokeStyle = gradientStroke;
+
+		ctx.beginPath();
+		ctx.arc(x, y, cw * 0.02, start, diff/10+start, false);
+		ctx.stroke();
+		// ctx.fill();
 	}, 1000);
 	setTimeout(() => {
 		wrapper.removeChild(wrapper.lastChild);
