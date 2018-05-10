6bff14316aab968257d314ce0c0bb4c49fc97b93
chapay <BritikovKI@Yandex.ru>
2018-05-10 11:30:21 +0300

    fixes( That feeling, when you thought, that you have avenged Egor, but then fixed all your mistakes yourself)

--- public/js/modules/ws.js
+++ public/js/modules/ws.js
@@ -6,7 +6,7 @@ export default class Ws {
 		if (Ws.__instance) {
 			return Ws.__instance;
 		}
-		const address = 'wss://localhost:5000/multiplayer/rand';
+		const address = 'wss://rha-backend.herokuapp.com/multiplayer/rand';
 		this.ws = new WebSocket(address);
 		this.ws.onopen = (event) => {
 			console.log(`WebSocket on address ${address} opened`);
