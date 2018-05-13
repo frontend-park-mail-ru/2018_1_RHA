7732269bdf7036b7f09485f6a704dff2dd211d29
supreme2302 <chuvashov.egor@mail.ru>
2018-05-13 16:48:20 +0300

    wss

--- public/js/components/views/multiplayerView/multiplayerView.js
+++ public/js/components/views/multiplayerView/multiplayerView.js
@@ -12,7 +12,22 @@ export default class MultiplayerSection extends Section {
 		this.parent = document.getElementById('game');
 		this.wrapper = document.createElement('div');
 		this.wrapper.classList.add('wrapper');
-		//todo поменять где надо айдишники
+		this.winWidth = null;
+		this.winHeight = null;
+		this.winWidth = window.innerWidth;
+		this.winHeight = this.winWidth * 0.5625; //соотношение 16:9
+		//todo упростить
+		if (window.innerHeight > window.innerWidth) {
+			this.height_canv = window.innerWidth * 0.85; //максимально возможная высота канваса
+		}
+		else {
+			this.height_canv = window.innerHeight * 0.85;
+		}
+
+		if (this.winHeight >= window.innerHeight) {
+			this.winHeight = window.innerHeight;
+			//this.height_canv = this.winHeight * 0.85;
+		}
 
 		this.wrapper.innerHTML += generateCanvas(
 			{
@@ -26,6 +41,13 @@ export default class MultiplayerSection extends Section {
 		this.parent.appendChild(this.wrapper);
 		this.game_canvas = document.getElementById('multiplayer-canvas');
 		this.ctx = this.game_canvas.getContext('2d');
+		this.beforeResize = {
+			width: window.innerWidth,
+			height: window.innerHeight
+		};
+		this.game_canvas.style.marginTop = String(100 - 100 * this.game_canvas.height / this.height_canv) / 2 + '%';
+
+
 	}
 
 	render() {
@@ -41,6 +63,7 @@ export default class MultiplayerSection extends Section {
 			.then(
 				() => {
 					this.setWindowResizeHandler();
+					this.listenOrientation();
 					this.coordinate = new Coordinate(this.game_canvas);
 					this.changeBut = this.wrapper.getElementsByClassName('change')[0];
 					this.game = new Game(GameModes.multiplayer, this.game_canvas, this.coordinate, this.changeBut, this.img);
@@ -61,11 +84,54 @@ export default class MultiplayerSection extends Section {
 		return [window.innerWidth * 0.7, window.innerWidth * 0.525 * 0.83];
 	}
 
+	listenOrientation() {
+		let mql = window.matchMedia('(orientation: portrait)');
+		if (mql.matches) {
+			const overlayOrientation = document.createElement('div');
+			this.wrapper.appendChild(overlayOrientation);
+			overlayOrientation.classList.add('overlayOrientation');
+			overlayOrientation.innerText = 'Change to the landscape orientation';
+		}
+		mql.addListener(m => {
+			if (m.matches) {
+				const overlayOrientation = document.createElement('div');
+				this.wrapper.appendChild(overlayOrientation);
+				overlayOrientation.classList.add('overlayOrientation');
+				overlayOrientation.innerText = 'Change to the landscape orientation';
+
+			}
+			else {
+				this.height_canv = window.innerHeight * 0.85;
+				this.wrapper.removeChild(this.wrapper.lastChild);
+			}
+		});
+	}
+
 	setWindowResizeHandler() {
 		window.addEventListener('resize', () => {
-			[this.game_canvas.width, this.game_canvas.height] = this.computeCanvasSize();
+			let sizes = this.computeCanvasSize();
+			if (sizes[1] >= window.innerHeight * 0.85) {
+				sizes[1] = window.innerHeight * 0.85;
+			}
+			[this.game_canvas.width, this.game_canvas.height] = sizes;
 			this.ctx.drawImage(this.img, 0, 0, this.game_canvas.width, this.game_canvas.height);
 			this.coordinate.reSize(this.game_canvas);
+			let margin = 100 - 100 * this.game_canvas.height / this.height_canv;
+			if (margin <= 0) {
+				margin = 0;
+			}
+			if (this.game_canvas.width !== this.beforeResize.width) {
+				this.game_canvas.style.marginTop = String(margin) / 2 + '%';
+			}
+			else {
+				this.game_canvas.style.marginTop = String(margin) / 2 + '%';
+				this.height_canv = window.innerHeight * 0.85;
+			}
+			this.beforeResize = {
+				width: this.game_canvas.width,
+				height: this.game_canvas.height
+			};
+
 			bus.emit('resize-for-draw', {});
 		});
 		return this;
