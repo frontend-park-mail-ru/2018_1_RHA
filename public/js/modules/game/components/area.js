2472aa7a45dcc51e8bd54f5a5295f017427994f9
HimmelSpark <adam404pet@gmail.com>
2018-05-20 19:18:26 +0300

    э

--- public/js/modules/game/components/area.js
+++ public/js/modules/game/components/area.js
@@ -14,21 +14,28 @@ export default class Area {
 	}
 
 	init() {
-		let X = this.coordinate.R * 2 + this.coordinate.R * 2 * this.coordinate.I;
-		let Y = 0;
-		if (this.coordinate.J % 2 === 0) {
-			Y = this.coordinate.R * 2 * 0.866 + this.coordinate.J * this.coordinate.R * 0.866;
-		} else {
-			Y = this.coordinate.R * 0.866 + this.coordinate.J * this.coordinate.R * 0.866;
-		}
+		// let X = this.coordinate.R * 2 + this.coordinate.R * 2 * this.coordinate.I;
+		// let Y = 0;
+		// if (this.coordinate.J % 2 === 0) {
+		// 	Y = this.coordinate.R * 2 * 0.866 + this.coordinate.J * this.coordinate.R * 0.866;
+		// } else {
+		// 	Y = this.coordinate.R * 0.866 + this.coordinate.J * this.coordinate.R;
+		// }
+		// PS получено опытным путем
+		let sx = this.coordinate.R * 1.8;
+		let sy = this.coordinate.R * 1.05;
+		let x = this.coordinate.I * sx + 345;
+		let y = this.coordinate.J * sy * 2 + this.coordinate.I % 2 * sy + this.coordinate.R * 1.5;
 
 		this.area = new Kexagon(
 			this.name,
 			this.canvas,
-			X,
-			Y,
+			x,
+			y,
 			this.coordinate.R,
 			this.color
 		);
 	}
 }
+
+
