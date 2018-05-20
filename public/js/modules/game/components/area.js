f38d44fabde31ac73802ccab76a4eb382b4c352c
HimmelSpark <adam404pet@gmail.com>
2018-05-20 19:48:06 +0300

    perfetta letizia

--- public/js/modules/game/components/area.js
+++ public/js/modules/game/components/area.js
@@ -22,10 +22,19 @@ export default class Area {
 		// 	Y = this.coordinate.R * 0.866 + this.coordinate.J * this.coordinate.R;
 		// }
 		// PS получено опытным путем
-		let sx = this.coordinate.R * 1.8;
-		let sy = this.coordinate.R * 1.05;
-		let x = this.coordinate.I * sx + 345;
-		let y = this.coordinate.J * sy * 2 + this.coordinate.I % 2 * sy + this.coordinate.R * 1.5;
+		// let sx = this.coordinate.R * 1.8;
+		// let sy = this.coordinate.R * 1.05;
+		// let x = this.coordinate.I * sx + 345;
+		// let y = this.coordinate.J * sy * 2 + this.coordinate.I % 2 * sy + this.coordinate.R * 1.5;
+
+		let sx = this.coordinate.R * 3 / 2.0;
+		let sy = this.coordinate.R * Math.sqrt(3.0) / 2;
+		let x = this.coordinate.I * sx;
+		let y = this.coordinate.J * sy * 2 + this.coordinate.I % 2 * sy;
+		let dx = 300 * this.canvas.width / 1000;
+		let dy = this.coordinate.R * 1.5 * this.canvas.height / 610;
+		x = this.canvas.width / 1000 * x + dx;
+		y = this.canvas.height / 610 * y + dy;
 
 		this.area = new Kexagon(
 			this.name,
