d6e4487a2a5f3a177dfb400ce917031f6d2d50d0
supreme2302 <chuvashov.egor@mail.ru>
2018-05-21 22:06:39 +0300

    a

--- public/js/modules/game/gameScene.js
+++ public/js/modules/game/gameScene.js
@@ -276,18 +276,14 @@ export default class GameScene {
 				const coordinates = data.payload;
 				const curRegion = this.isRegion(coordinates.x, coordinates.y);
 
-				console.log('0');
 
 				if (!curRegion) {
 					return;
 				}
-				console.log('1  -  ', this.curPlayer );
-				this.curPlayer.showRegion();
 
 				if (!this.curPlayer.isTheRegionOfPlayer(curRegion)) {
 					return;
 				}
-				console.log('2');
 
 				aboutRegion(curRegion, this.about_region);
 
