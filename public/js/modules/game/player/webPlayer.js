de5917274f60f17930338d8d5d888a160b46587c
HimmelSpark <adam404pet@gmail.com>
2018-05-24 18:37:18 +0300

    merged:

--- public/js/modules/game/player/webPlayer.js
+++ public/js/modules/game/player/webPlayer.js
@@ -1,10 +1,8 @@
 import Player from './player.js';
 import bus from '../../bus.js';
-import MainPlayer from './mainPlayer';
 import {renderScene} from '../helperFuncs/renderScene';
 import {attackAnimation} from '../animation/attack/attackAnimation';
 import {moveAnimation} from '../animation/move/moveAnimation.js';
-import GameScene from '../gameScene.js';
 
 /**
  * Class representing web player
