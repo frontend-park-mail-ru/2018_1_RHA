2899e276d0ac776007f3a177ad7189e8338e2972
supreme2302 <chuvashov.egor@mail.ru>
2018-05-25 14:42:37 +0300

    sw

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlagin = require('copy-webpack-plugin');
 module.exports = {
 	devtool: 'source-map',
 	entry: './src/index.js',
@@ -48,6 +49,10 @@ module.exports = {
 			// both options are optional
 			filename: '[name].css',
 			chunkFilename: '[id].css'
-		})
+		}),
+		new CopyWebpackPlagin([{
+			from: path.join(__dirname, 'public', 'sw.js'),
+			to: path.join(__dirname, 'public/dist', 'sw.js')
+		}])
 	]
 };
